3c1addf4c389c4b9fe80e2c91e7f569809456973
ArminasGriesius <arminas.griesius@gmail.com>
2023-07-28 13:30:28 +0300

    3 uzd

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,5 +10,51 @@ Paspaudus mygtuką "Show users":
 Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi 
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
+"use strict";
+console.log("script.js file was loaded");
 
-const ENDPOINT = 'https://api.github.com/users';
+("use strict");
+
+const ENDPOINT = "https://api.github.com/users";
+
+const els = {
+  btnEl: document.getElementById("btn"),
+  message: document.getElementById("message"),
+  output: document.getElementById("output"),
+};
+
+let mainPostArr = [];
+
+els.btnEl.addEventListener("click", getUsers);
+
+async function getUsers() {
+  try {
+    const resp = await fetch(ENDPOINT);
+    const users = await resp.json();
+    mainPostArr = users;
+    render();
+  } catch (error) {
+    console.error("ivyko klaida:", error);
+  }
+}
+
+function render() {
+  els.output.innerHTML = "";
+  const htmlElArr = mainPostArr.map((obj) => makeOnePostEl(obj));
+  els.output.append(...htmlElArr);
+}
+
+function makeOnePostEl(obj) {
+  const ulEl = document.createElement("ul");
+  ulEl.classList.add("userList");
+  const liEl = document.createElement("li");
+  const h2El = document.createElement("h2");
+  h2El.textContent = obj.login;
+  const avatarEl = document.createElement("img");
+  avatarEl.src = obj.avatar_url;
+  avatarEl.alt = obj.login;
+  avatarEl.classList.add("avatar");
+  liEl.append(h2El, avatarEl);
+  ulEl.append(liEl);
+  return ulEl;
+}
