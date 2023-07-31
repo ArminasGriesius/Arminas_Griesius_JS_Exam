81bbdcae8be805c2a13f380e2dae5f9fe829db0e
ArminasGriesius <arminas.griesius@gmail.com>
2023-07-31 12:48:08 +0300

    9 uzd

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,35 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+"use strict";
+console.log("script.js file was loaded");
+
+class Movie {
+  constructor(argTitle, argDirector, argBudget) {
+    this.title = argTitle;
+    this.director = argDirector;
+    this.budget = argBudget;
+  }
+  wasExpensive() {
+    if (this.budget >= 100000000) {
+      console.log("Expensive movie indeed");
+      return console.log(true);
+    } else {
+      console.log("Cheap movie");
+      return console.log(false);
+    }
+  }
+}
+
+const cheapMovie = new Movie("Rocky", "John G. Avildsen", 960000);
+console.log("cheapMovie ===", cheapMovie);
+cheapMovie.wasExpensive();
+
+const expensiveMovie = new Movie(
+  "Harry Potter and the Half-Blood Prince",
+  "David Yates",
+  250000000
+);
+console.log("cheapMovie ===", expensiveMovie);
+expensiveMovie.wasExpensive();
